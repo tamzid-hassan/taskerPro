c442698390ed460a20afd974c8f440a1d6c89af1
tamzid.hassan <iamtamzid96+github@gmail.com>
2024-09-24 02:25:09 +0600

    Added Daisy UI plugin and framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { motion } from "framer-motion"
+
 function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-bold'>Test</h1>
+      <h1 className='text-3xl font-bold text-secondary'>Test</h1>
+      <motion.button
+        whileHover={{
+          rotate: "-25deg",
+          scale: 1.1
+        }}
+        whileTap={{
+          rotate: "10deg",
+          scale: 0.9
+        }}
+        className="btn btn-primary ">Button</motion.button>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,36 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  // add daisyUI plugin
+  plugins: [require("daisyui")],
+
+  // daisyUI config (optional - here are the default values)
+  daisyui: {
+    themes: [{
+      taskerProLight: {
+        "primary": "#66a969",
+        "secondary": "#a7cdbf",
+        "accent": "#80b7ae",
+        "neutral": "#3d4451",
+        "base-100": "#f9fbf9",
+      }
+    }, {
+      taskerProDark: {
+        "primary": "#56995a",
+        "secondary": "#32584a",
+        "accent": "#487f76",
+        "neutral": "#3d4451",
+        "base-100": "#061106",
+      }
+    }, "winter"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "taskerProDark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
+
 }
 
